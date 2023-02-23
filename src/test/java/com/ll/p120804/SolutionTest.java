9308e1a4fe80cc5151b3d2316a32b15f90830f5f
yurrrrri <yu_ri_son@naver.com>
2023-02-23 09:34:01 +0900

    알고리즘 풀이

---
+++ src/test/java/com/ll/p120804/SolutionTest.java
@@ -0,0 +1,19 @@
+package com.ll.p120804;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SolutionTest {
+    @Test
+    @DisplayName("3, 4 => 12")
+    void t1(){
+        assertThat(new Solution().solution(3, 4)).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("27, 19 => 513")
+    void t2(){
+        assertThat(new Solution().solution(27, 19)).isEqualTo(513);
+    }
+}

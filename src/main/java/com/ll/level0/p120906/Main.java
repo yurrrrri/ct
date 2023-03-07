1a45376754db5c56f98f8280309e8dac6dfe7ef9
yurrrrri <yu_ri_son@naver.com>
2023-03-07 09:42:21 +0900

    자릿수 더하기 문제 해결

---
+++ src/main/java/com/ll/level0/p120906/Main.java
@@ -0,0 +1,15 @@
+package com.ll.level0.p120906;
+
+public class Main {
+}
+
+class Solution{
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}

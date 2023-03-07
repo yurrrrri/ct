ff17b4b6a232254f6493d9ca2008b07e76112868
yurrrrri <yu_ri_son@naver.com>
2023-03-07 09:49:15 +0900

    개미 군단 문제 해결

---
+++ src/main/java/com/ll/level0/p120837/Main.java
@@ -0,0 +1,23 @@
+package com.ll.level0.p120837;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        while(hp>=5){
+            hp-=5;
+            answer++;
+        }
+        while(hp>=3){
+            hp-=3;
+            answer++;
+        }
+        while(hp>0){
+            hp--;
+            answer++;
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120837/MainTest.java
@@ -0,0 +1,26 @@
+package com.ll.level0.p120837;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    @DisplayName("23 => 5")
+    void t1(){
+        assertThat(new Solution().solution(23)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("24 => 6")
+    void t2(){
+        assertThat(new Solution().solution(24)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("999 => 201")
+    void t3(){
+        assertThat(new Solution().solution(999)).isEqualTo(201);
+    }
+}

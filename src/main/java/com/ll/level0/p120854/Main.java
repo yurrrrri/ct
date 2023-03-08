bbe5cd249f2c45fa635f780347a157320cb3e10a
yurrrrri <yu_ri_son@naver.com>
2023-03-08 14:43:09 +0900

    배열 원소의 길이 문제 해결

---
+++ src/main/java/com/ll/level0/p120854/Main.java
@@ -0,0 +1,14 @@
+package com.ll.level0.p120854;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i=0; i<strlist.length; i++){
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}

00d547af650d961751fab55d15c8af2fd791a57a
yurrrrri <yu_ri_son@naver.com>
2023-03-09 09:25:40 +0900

    중복된 숫자 개수 문제 해결

---
+++ src/main/java/com/ll/level0/p120583/Main.java
@@ -0,0 +1,14 @@
+package com.ll.level0.p120583;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for(int number : array){
+            if(number == n) answer++;
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120583/MainTest.java
@@ -0,0 +1,22 @@
+package com.ll.level0.p120583;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    @DisplayName("{1, 1, 2, 3, 4, 5} => 2")
+    void t1(){
+        int[] array = {1, 1, 2, 3, 4, 5};
+        assertThat(new Solution().solution(array, 1)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("{0, 2, 3, 4} => 0")
+    void t2(){
+        int[] array = {0, 2, 3, 4};
+        assertThat(new Solution().solution(array, 1)).isEqualTo(0);
+    }
+}

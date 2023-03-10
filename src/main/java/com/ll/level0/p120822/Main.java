228f316a120068d0e85ec0d42d56925ffb832718
yurrrrri <yu_ri_son@naver.com>
2023-03-10 09:39:06 +0900

    ë¯문제 해결

---
+++ src/main/java/com/ll/level0/p120822/Main.java
@@ -0,0 +1,14 @@
+package com.ll.level0.p120822;
+
+public class Main {
+}
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=my_string.length()-1; i>=0; i--){
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120889/MainTest.java
@@ -0,0 +1,29 @@
+package com.ll.level0.p120889;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    @DisplayName("[1, 2, 3]")
+    void t1(){
+        int[] sides = {1, 2, 3};
+        assertThat(new Solution().solution(sides)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("[3, 6, 2]")
+    void t2(){
+        int[] sides = {3, 6, 2};
+        assertThat(new Solution().solution(sides)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("[199, 72, 222]")
+    void t3(){
+        int[] sides = {199, 72, 222};
+        assertThat(new Solution().solution(sides)).isEqualTo(1);
+    }
+}

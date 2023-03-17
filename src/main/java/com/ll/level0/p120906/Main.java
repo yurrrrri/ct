e1c69a155547a184047dac6a1493f304b2a10f87
yurrrrri <yu_ri_son@naver.com>
2023-03-17 18:06:05 +0900

    level1

--- src/main/java/com/ll/level0/p120906/Main.java
+++ src/main/java/com/ll/level0/p120906/Main.java
@@ -1,9 +1,6 @@
 package com.ll.level0.p120906;
 
-public class Main {
-}
-
-class Solution{
+public class Solution{
     public int solution(int n) {
         int answer = 0;
         while (n > 0) {

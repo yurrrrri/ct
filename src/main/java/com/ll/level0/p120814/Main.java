b6bc5e13cba94541991cdd3f265f6f3a0ea8a0a0
yurrrrri <yu_ri_son@naver.com>
2023-03-29 09:28:46 +0900

    p120814

---
+++ src/main/java/com/ll/level0/p120814/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level0.p120814;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(int n) {
+        return n%7 > 0 ? n/7+1 : n/7;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120814/MainTest.java
@@ -0,0 +1,23 @@
+package com.ll.level0.p120814;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+
+    @Test
+    void t1(){
+        assertThat(new Solution().solution(7)).isEqualTo(1);
+    }
+
+    @Test
+    void t2(){
+        assertThat(new Solution().solution(1)).isEqualTo(1);
+    }
+
+    @Test
+    void t3(){
+        assertThat(new Solution().solution(15)).isEqualTo(3);
+    }
+}

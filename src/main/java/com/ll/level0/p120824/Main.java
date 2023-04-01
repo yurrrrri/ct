1edab15cb594d2b35d4be0ab4ada23c016b134a0
yurrrrri <yu_ri_son@naver.com>
2023-04-01 13:01:00 +0900

    p120824

---
+++ src/main/java/com/ll/level0/p120824/Main.java
@@ -0,0 +1,18 @@
+package com.ll.level0.p120824;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for(int num : num_list){
+            if(num%2 == 0){
+                answer[0] = answer[0] + 1;
+            } else {
+                answer[1] = answer[1] + 1;
+            }
+        }
+        return answer;
+    }
+}

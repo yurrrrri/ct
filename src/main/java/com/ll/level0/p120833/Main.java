c62e656948ba07d570ab2c011df8321d778078d2
yurrrrri <yu_ri_son@naver.com>
2023-04-10 16:03:42 +0900

    p120833

---
+++ src/main/java/com/ll/level0/p120833/Main.java
@@ -0,0 +1,16 @@
+package com.ll.level0.p120833;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        int index = 0;
+        for(int i=num1; i<=num2; i++){
+            answer[index] = numbers[i];
+            index++;
+        }
+        return answer;
+    }
+}

66df41fcc0f069b2167a0874cbe2ff53d3691ae4
yurrrrri <yu_ri_son@naver.com>
2023-04-12 15:25:41 +0900

    p120847

---
+++ src/main/java/com/ll/level0/p120847/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p120847;
+
+import java.util.Arrays;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
+    }
+}

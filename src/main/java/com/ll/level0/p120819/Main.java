4ed3911d73116bc77ba14cf1bb2763f48484faf4
yurrrrri <yu_ri_son@naver.com>
2023-04-14 13:02:21 +0900

    p120819

---
+++ src/main/java/com/ll/level0/p120819/Main.java
@@ -0,0 +1,11 @@
+package com.ll.level0.p120819;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {money/5500, money%5500};
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120819/MainTest.java
@@ -0,0 +1,19 @@
+package com.ll.level0.p120819;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    void t1(){
+        int[] result = {1, 0};
+        assertThat(new Solution().solution(5500)).isEqualTo(result);
+    }
+
+    @Test
+    void t2(){
+        int[] result = {2, 4000};
+        assertThat(new Solution().solution(15000)).isEqualTo(result);
+    }
+}

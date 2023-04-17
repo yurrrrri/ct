1567ef43e3b4f13e48bd9c5abcdd31370b912da7
yurrrrri <yu_ri_son@naver.com>
2023-04-17 15:54:01 +0900

    p87377 transformToMatrix

--- src/main/java/com/ll/level2/p87377/Main.java
+++ src/main/java/com/ll/level2/p87377/Main.java
@@ -1,7 +1,9 @@
 package com.ll.level2.p87377;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 }
@@ -34,20 +36,74 @@ class Solution {
         return Point.of(x, y);
     }
 
-    public List<Point> intersections(int[][] line) {
-        List<Point> points = new ArrayList<>();
+    public Set<Point> intersections(int[][] line) {
+        Set<Point> points = new HashSet<>();
 
         for(int i = 0; i < line.length; i++){
-            for(int j = 1; j < line.length; j++){
+            for(int j = i+1; j < line.length; j++){
                 int[] line1 = line[i];
                 int[] line2 = line[j];
 
                 Point point = intersection(line1, line2);
-                if(point != null) { points.add(point); }
+                if(point != null) points.add(point);
             }
         }
 
         return points;
+    }
+
+    public Point getMinPoint(Set<Point> points) {
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for(Point point : points) {
+            x = Math.min(x, point.x);
+            y = Math.min(y, point.y);
+        }
+
+        return Point.of(x, y);
+    }
+
+    public Point getMaxPoint(Set<Point> points) {
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE;
+
+        for(Point point : points) {
+            x = Math.max(x, point.x);
+            y = Math.max(y, point.y);
+        }
+
+        return Point.of(x, y);
+    }
+
+    public char[][] emptyMatrix(Set<Point> points) {
+        Point minPoint = getMinPoint(points);
+        Point maxPoint = getMaxPoint(points);
+
+        int width = (int)(maxPoint.x - minPoint.x + 1);
+        int height = (int)(maxPoint.y - minPoint.y + 1);
+
+        char[][] matrix = new char[height][width];
+        Arrays.stream(matrix).forEach(row -> Arrays.fill(row, '.'));
+
+        return matrix;
+    }
+
+    public Set<Point> positivePoints(Set<Point> points) {
+        Point minPoint = getMinPoint(points);
+
+        return points.stream()
+                .map(p -> Point.of(p.x - minPoint.x, p.y - minPoint.y))
+                .collect(Collectors.toSet());
+    }
+
+    public char[][] transformToMatrix(Set<Point> points) {
+        char[][] matrix = emptyMatrix(points);
+        points = positivePoints(points);
+
+        points.forEach(p -> matrix[(int) p.y][(int) p.x] = '*');
+
+        return matrix;
     }
 }
 
@@ -77,5 +133,20 @@ class Point {
 
         if (x != point.x) return false;
         return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (x ^ (x >>> 32));
+        result = 31 * result + (int) (y ^ (y >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
--- src/test/java/com/ll/level2/p87377/MainTest.java
+++ src/test/java/com/ll/level2/p87377/MainTest.java
@@ -2,15 +2,50 @@ package com.ll.level2.p87377;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainTest {
 
     @Test
-    void intersection1(){
+    void intersection(){
         int[] line1 = {1, -1, 0};
         int[] line2 = {2, -1, 0};
         assertThat(new Solution().intersection(line1, line2)).isEqualTo(Point.of(0, 0));
+    }
+
+    @Test
+    void intersections(){
+        int[][] line = {{1, -1, 0}, {2, -1, 0}, {4, -1, 0}};
+        assertThat(new Solution().intersections(line)).isEqualTo(Set.of(Point.of(0, 0)));
+    }
+
+    @Test
+    void getMinPoint(){
+        assertThat(new Solution().getMinPoint(Set.of(Point.of(1, -1), Point.of(-1, 1)))).isEqualTo(Point.of(-1, -1));
+    }
+
+    @Test
+    void getMaxPoint(){
+        assertThat(new Solution().getMaxPoint(Set.of(Point.of(4, -1), Point.of(-1, 6)))).isEqualTo(Point.of(4, 6));
+    }
+
+    @Test
+    void emptyMatrix(){
+        assertThat(new Solution().emptyMatrix(Set.of(Point.of(1, 1), Point.of(-1, 1))))
+                .isEqualTo(new char[][]{{'.', '.', '.'}});
+    }
+
+    @Test
+    void positivePoints(){
+        assertThat(new Solution().positivePoints(Set.of(Point.of(4, -7), Point.of(-1, 6)))).isEqualTo(Set.of(Point.of(5, 0), Point.of(0, 13)));
+    }
+
+    @Test
+    void transformToMatrix(){
+        assertThat(new Solution().transformToMatrix(Set.of(Point.of(1, 1), Point.of(-1, 1))))
+                .isEqualTo(new char[][]{{'*', '.', '*'}});
     }
 
     @Test

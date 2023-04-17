5ab23b474992b9efca91db555a3e515b1274b3a9
yurrrrri <yu_ri_son@naver.com>
2023-04-17 09:43:07 +0900

    p120809

---
+++ src/main/java/com/ll/level0/p120809/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p120809;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        for(int i=0; i<numbers.length; i++){
+            numbers[i] *= 2;
+        }
+        return numbers;
+    }
+}
---
+++ src/main/java/com/ll/level0/p120898/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level0.p120898;
+
+public class Main {
+}
+
+class Solution {
+    public int solution(String message) {
+        return message.length() * 2;
+    }
+}

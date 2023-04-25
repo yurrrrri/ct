62b19a3a70a8749d604b90e94b1b041ae4ff4f2e
yurrrrri <yu_ri_son@naver.com>
2023-04-25 09:55:44 +0900

    p42862 complete

--- src/main/java/com/ll/level1/p42862/Main.java
+++ src/main/java/com/ll/level1/p42862/Main.java
@@ -1,11 +1,34 @@
 package com.ll.level1.p42862;
 
+import java.util.HashSet;
+
 public class Main {
 }
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        return answer;
+        HashSet<Integer> reserveSet = new HashSet<>();
+        HashSet<Integer> lostSet = new HashSet<>();
+
+        for(int l : lost) {
+            lostSet.add(l);
+        }
+        for(int r : reserve) {
+            if(lostSet.contains(r)) {
+                lostSet.remove(r);
+            } else {
+                reserveSet.add(r);
+            }
+        }
+
+        for (int i : reserveSet) {
+            if (lostSet.contains(i - 1)) {
+                lostSet.remove(i - 1);
+            } else if (lostSet.contains(i + 1)) {
+                lostSet.remove(i + 1);
+            }
+        }
+
+        return n - lostSet.size();
     }
 }
--- src/test/java/com/ll/level1/p42862/SolutionTest.java
+++ src/test/java/com/ll/level1/p42862/SolutionTest.java
@@ -1,4 +1,28 @@
 package com.ll.level1.p42862;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SolutionTest {
+
+    @Test
+    void t001() {
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        assertThat(new Solution().solution(5, lost, reserve)).isEqualTo(5);
+    }
+
+    @Test
+    void t002() {
+        int[] lost = {2, 4};
+        int[] reserve = {3};
+        assertThat(new Solution().solution(5, lost, reserve)).isEqualTo(4);
+    }
+
+    @Test
+    void t003() {
+        int[] lost = {3};
+        int[] reserve = {1};
+        assertThat(new Solution().solution(3, lost, reserve)).isEqualTo(2);
+    }
 }

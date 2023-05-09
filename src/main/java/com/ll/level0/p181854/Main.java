7d8b429539db2a582a2462194585bec55674cb18
yurrrrri <yu_ri_son@naver.com>
2023-05-09 10:58:15 +0900

    p181854

---
+++ src/main/java/com/ll/level0/p181854/Main.java
@@ -0,0 +1,20 @@
+package com.ll.level0.p181854;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int[] arr, int n) {
+        int len = arr.length;
+        if(len % 2 != 0){
+            for(int i=0; i<len; i+=2){
+                arr[i] = arr[i] + n;
+            }
+        } else {
+            for(int i=1; i<len; i+=2){
+                arr[i] = arr[i] + n;
+            }
+        }
+        return arr;
+    }
+}

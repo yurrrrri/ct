645c369a43cdd310deac8e877a000eb0aecc22f9
yurison <yu_ri_son@naver.com>
2023-08-03 12:21:32 +0900

    p12922

---
+++ src/main/java/com/ll/level1/p12922/Main.java
@@ -0,0 +1,15 @@
+package com.ll.level1.p12922;
+
+public class Main {
+}
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<n; i++) {
+            if (i % 2 == 0) sb.append("수");
+            else sb.append("박");
+        }
+        return sb.toString();
+    }
+}

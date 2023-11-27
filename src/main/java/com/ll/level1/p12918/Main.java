c83437b03a4c491011b73b630619966a4c78d0dd
yurrrrri <yu_ri_son@naver.com>
2023-11-27 19:40:52 +0900

    p12918 fix

--- src/main/java/com/ll/level1/p12918/Main.java
+++ src/main/java/com/ll/level1/p12918/Main.java
@@ -7,10 +7,8 @@ class Solution {
     public boolean solution(String s) {
         if (s.length() != 4 && s.length() != 6) return false;
 
-        char[] arr = s.toCharArray();
-
-        for (char c : arr) {
-            if ((c - '0') > 9) return false;
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
         }
 
         return true;

6698a28dada0c59e0298d53ce5e528d74e1ccd93
yurrrrri <yu_ri_son@naver.com>
2023-11-27 19:34:28 +0900

    p81301 fix

--- src/main/java/com/ll/level1/p81301/Main.java
+++ src/main/java/com/ll/level1/p81301/Main.java
@@ -7,11 +7,10 @@ class Solution {
     public int solution(String s) {
         String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        for (int i = 0; i < words.length; i++) {
-            if (s.contains(words[i])) {
-                s = s.replace(words[i], Integer.toString(i));
-            }
+        for(int i = 0; i < words.length; i++){
+            s = s.replace(words[i], Integer.toString(i));
         }
+
         return Integer.parseInt(s);
     }
 }
